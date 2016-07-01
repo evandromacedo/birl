a586d5e21b6cc23008642f34e6cb5d3d22b6124d
Evandro Macedo <evandro.hcm@gmail.com>
2016-07-01 20:30:09 -0300

    Criando outro arquivo pras funções

---
+++ js/functions.js
@@ -0,0 +1,108 @@
+var body            = $("body"),
+    birlButton      = $("#birlButton"),
+    domBirlCounter  = $("#birlCounter"),
+    domBirlLevel    = $("#birlLevel"),
+    domBirlTimer    = $("#birlTimer"),
+    modal           = $('#modal'),
+    nextLevelButton = $('#nextLevelButton'),
+    nextLevelText   = $('#nextLevelText'),
+    nextLevelSpan   = $('#nextLevelSpan');
+
+var openModal = function(text, level) {
+    modal.css('display', 'block');
+    nextLevelText.text(text);
+    nextLevelSpan.text(level);
+};
+
+var closeModal = function() {
+    modal.css('display', 'none');
+};
+
+var setTimerRed = function() {
+    domBirlTimer.css('color', 'red');
+};
+
+var setTimerBlack = function() {
+    domBirlTimer.css('color', 'black');
+};
+
+var changeScenario = function(scenario) {
+    switch (scenario) {
+        case 'academia':
+            body.css({
+                'background': 'url(img/cenarios/cenario_academia.jpg) repeat-x center bottom',
+                'backgorund-color': '#B4F8F9'
+            });
+            break;
+
+        case 'praia':
+            body.css({
+                'background': 'url(img/cenarios/cenario_praia.jpg) repeat-x center bottom',
+                'backgorund-color': '#FFF'
+            });
+            break;
+    }
+};
+
+var toggleBirlClick = function() {
+    birlButton.css({
+        'pointer-events': 'none',
+        'background'    : 'url(img/bodybuilder2_peso2.png) no-repeat 100% 100%'
+    });
+
+    setTimeout(function() {
+
+        birlButton.css({
+            'pointer-events': 'auto',
+            'background'    : 'url(img/bodybuilder2_peso.png) no-repeat 100% 100%'
+        });
+
+    }, 1000);
+};
+
+var disableBambam = function() {
+    birlButton.css({
+        'pointer-events': 'none',
+        'background'    : 'url(img/bodybuilder2_peso3.png) no-repeat 100% 100%'
+    });
+};
+
+var enableBambam = function() {
+    birlButton.css({
+        'pointer-events': 'auto',
+        'background'    : 'url(img/bodybuilder2_peso.png) no-repeat 100% 100%'
+    });
+};
+
+
+/* -----------------------------------------------
+    Gambiarra véa de Zé que depois eu ajeito
+-------------------------------------------------- */
+
+var size = 0;
+$("#barra").css('height', size);
+
+function addBar(){
+    size += 10;
+
+    if (size > 40 && size <= 80){$("#barra").css('background-color', '#FF0'); }
+    else if (size > 80){ $("#barra").css('background-color', '#F00'); }
+    else{  $("#barra").css('background-color', '#0F0'); }
+
+    $("#barra").css('height', size+'%');
+}
+function rmvBar(){
+    size -= 10;
+
+
+    if (size > 260 && size <= 400){$("#barra").css('background-color', '#FF0'); }
+    else if (size > 400){ $("#barra").css('background-color', '#F00'); }
+    else{  $("#barra").css('background-color', '#0F0'); }
+
+    $("#barra").css('height', size+'%');
+}
+function clrBar(){
+    size = 0;
+    $("#barra").css('height', size+'%');
+    $("#barra").css('background-color', '#0F0');
+}
--- js/main.js
+++ js/main.js
@@ -28,11 +28,6 @@ jQuery(document).ready(function($) {
           nextLevelText       = $('#nextLevelText'),
           nextLevelSpan       = $('#nextLevelSpan');
 
-    var openModal = function(text, level) {
-        modal.css('display', 'block');
-        nextLevelText.text(text);
-        nextLevelSpan.text(level);
-    };
 
     openModal();
 
@@ -55,7 +50,7 @@ jQuery(document).ready(function($) {
             }
 
             if (self.birlTimer == 5) {
-                domBirlTimer.css('color', 'red');
+                setTimerRed();
                 audioVaiDarNao.play();
             }
         }
@@ -125,7 +120,7 @@ jQuery(document).ready(function($) {
 
         self.playAgain = function() {
             clrBar();
-            domBirlTimer.css('color', 'black');
+            setTimerBlack();
             openModal('Perdeu. Mas um monstro nunca desiste', self.birlLevel);
             clearInterval(self.birlInterval);
         }
@@ -189,7 +184,7 @@ jQuery(document).ready(function($) {
         else
             bambam.playNextLevel();
 
-        modal.css('display', 'none');
+        closeModal();
     });
 
     birlButton.on('click', function() {
@@ -207,87 +202,6 @@ jQuery(document).ready(function($) {
 
         toggleBirlClick();
     });
-
-    var changeScenario = function(scenario) {
-        switch (scenario) {
-            case 'academia':
-                body.css({
-                    'background': 'url(img/cenarios/cenario_academia.jpg) repeat-x center bottom',
-                    'backgorund-color': '#B4F8F9'
-                });
-                break;
-
-            case 'praia':
-                body.css({
-                    'background': 'url(img/cenarios/cenario_praia.jpg) repeat-x center bottom',
-                    'backgorund-color': '#FFF'
-                });
-                break;
-        }
-    };
-
-    var toggleBirlClick = function() {
-        birlButton.css({
-            'pointer-events': 'none',
-            'background'    : 'url(img/bodybuilder2_peso2.png) no-repeat 100% 100%'
-        });
-
-        setTimeout(function() {
-
-            birlButton.css({
-                'pointer-events': 'auto',
-                'background'    : 'url(img/bodybuilder2_peso.png) no-repeat 100% 100%'
-            });
-
-        }, 1000);
-    };
-
-    var disableBambam = function() {
-        birlButton.css({
-            'pointer-events': 'none',
-            'background'    : 'url(img/bodybuilder2_peso3.png) no-repeat 100% 100%'
-        });
-    };
-
-    var enableBambam = function() {
-        birlButton.css({
-            'pointer-events': 'auto',
-            'background'    : 'url(img/bodybuilder2_peso.png) no-repeat 100% 100%'
-        });
-    };
-
-
-    /* -----------------------------------------------
-        Gambiarra véa de Zé que depois eu ajeito
-    -------------------------------------------------- */
-
-    var size = 0;
-    $("#barra").css('height', size);
-
-    function addBar(){
-        size += 10;
-
-        if (size > 40 && size <= 80){$("#barra").css('background-color', '#FF0'); }
-        else if (size > 80){ $("#barra").css('background-color', '#F00'); }
-        else{  $("#barra").css('background-color', '#0F0'); }
-
-        $("#barra").css('height', size+'%');
-    }
-    function rmvBar(){
-        size -= 10;
-
-
-        if (size > 260 && size <= 400){$("#barra").css('background-color', '#FF0'); }
-        else if (size > 400){ $("#barra").css('background-color', '#F00'); }
-        else{  $("#barra").css('background-color', '#0F0'); }
-
-        $("#barra").css('height', size+'%');
-    }
-    function clrBar(){
-        size = 0;
-        $("#barra").css('height', size+'%');
-        $("#barra").css('background-color', '#0F0');
-    }
 
 });
 
